076c7e29694d5607d310ff00adc9256b263b5ae5
Dave Rora <ftdrodriguezr@gmail.com>
2021-09-03 13:39:33 -0400

    NavBar basic style not responsive not icons not active option

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,22 @@
 export default function Navbar() {
   return (
-    <header className="bg-gray-800 container ">
-      <div className="py-8 px-4 flex flex-row justify-between">
-          <a href="#about" className="text-white border-r-2">
+    <header className="bg-gray-800 container mx-auto">
+      <div className="py-4 px-2 flex flex-row">
+          <a href="#about" className="text-white md:border-r-2 text-xl md:px-4 my-auto font-medium">
             David Rodriguez Ramirez
           </a>
-          <nav>
-            <a href="#projects" className="">
+          <nav className='justify-items-start flex-grow content-center flex gap-x-4 my-auto pl-4'>
+            <a href="#projects" className="hover:text-white">
               Past work
             </a>
-            <a href="#skills" className="">
+            <a href="#skills" className="hover:text-white">
               Skills
             </a>
-            <a href="#testimonials" className="">
+            <a href="#testimonials" className="hover:text-white">
               Testimonials
             </a>
           </nav>
-          <a href="#contact" className="">
+          <a href="#contact" className="hover:bg-gray-700 py-2 px-4 rounded-md">
             Hire Me
           </a>
       </div>
