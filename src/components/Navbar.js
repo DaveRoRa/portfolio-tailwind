a796ea1d50a78adf34fd7d03ef1eeaee032551db
Dave Rora <ftdrodriguezr@gmail.com>
2021-09-04 15:50:04 -0400

    Navbar Endad

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
+import { ArrowRightIcon } from '@heroicons/react/solid'
 export default function Navbar() {
   return (
-    <header className="bg-gray-800">
+    <header className="bg-gray-800 self-center md:sticky top-0 z-20">
       <div className="py-5 px-2 flex md:flex-row flex-col gap-y-3">
           <a href="#about" className="text-white md:border-r-2 text-xl md:px-4 md:my-auto self-center font-medium">
             David Rodriguez Ramirez
@@ -16,8 +17,8 @@ export default function Navbar() {
               Testimonials
             </a>
           </nav>
-          <a href="#contact" className="hover:bg-gray-700 py-1 px-2 md:py-2 md:px-4 rounded-md self-center">
-            Hire Me
+          <a href="#contact" className="hover:bg-gray-700 py-1 px-2 md:py-2 md:px-4 rounded-md self-center flex flex-row">
+            <span>Hire Me</span><ArrowRightIcon className='ml-2 w-5'/>
           </a>
       </div>
     </header>
